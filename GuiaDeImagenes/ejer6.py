e667e8075f7fbf1c6991b52dc0ba000ec1ac6535
estebanpc1918 <estebanpalacios170@gmail.com>
2024-08-13 11:50:50 -0300

    Create ejer6.py

---
+++ GuiaDeImagenes/ejer6.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+def crear_collage(imagenes,collague_size,collasal_path):
+    collage = Image.new('RGB',collague_size,(255,255,255))
+    img_width, ima_height = collague_size[0] // 2, collague_size[1] // 2
+    x,y = 0,0
+    
+
+
+crear_collage()
+
+collage_size = (800,800)
+collasal_path= 'collage.jpg'
+imagenes= ['img/messi1.jpeg','img/messi2.jpeg','img/messi3.jpeg','img/messi4.jpeg','img/messi5.jpeg','img/messi6.jpeg','img/messi7.jpeg','img/messi8.jpeg','img/messi9.jpeg']
